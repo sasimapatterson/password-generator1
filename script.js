13946c0aa4166cb30112d35e6689492c6a6379b4
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-17 17:24:26 -0400

    trying prompts for the password criterias. trying if-else statement but haven't got it to work yet

---
+++ script.js
@@ -0,0 +1,29 @@
+// Assignment Code
+//var generateBtn = document.querySelector("#generate");
+
+//function generatePassword() {
+  //var passwordlength = Number(prompt("How long would you like for your password?"));
+  //var lowerChar = prompt("Would you like lowercases in your password?");
+  //var upperCase = prompt("Would you like uppcase letters in your password?");
+  //var number = Number(prompt("Would you like numbers in your password?"));
+  //var specialChar = prompt("Would you like special characters in your password?");
+//}
+
+// Write password to the #password input
+//function writePassword() {
+ // var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
+  
+
+  //passwordText.value = password;
+
+//}
+
+// Add event listener to generate button
+//generateBtn.addEventListener("click", writePassword);
+
+var passwordLength = Number(prompt("Length pls?"));
+
+if (passwordLenght < 8) {
+    console.log("Longer please");
+}
