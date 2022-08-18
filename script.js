f8628f7f526efcc3ef25c571d83acd06831d29bd
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-18 17:18:19 -0400

    removed all conditional statements from every criteria and only used it at the end. made one more empty var to collect the possible characters from user. still trying to figure out how to get all the randome characters to display in the browser.

--- script.js
+++ script.js
@@ -2,50 +2,56 @@
 var generateBtn = document.querySelector("#generate");
 
 
-
-
 function generatePassword() {
-  //var number = confirm("Would you like numbers in your password?");
-  //var specialChar = confirm("Would you like special characters in your password?");
-
-  var numInput = "0123456789";
-  var specialInput = " ";
+  //
+  var lowercases = "abcdefghijklmnopqrstuvwxyz";
+  var uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var nums = "0123456789";
+  var symbols = "\" \"#$%&'()*+,-./:;<=>?@[\]^\`{|}~\" ";
+  var possibleChar = [];
   var finalPassword = [];
 
-var passwordLength = Number(prompt("How long would you like for your password?"));
-console.log(passwordLength);
-if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-  alert("Password must be between 8 and 12 characters");
-  return generatePassword();
-
-} 
-//else if (passwordLength > 8 || passwordLength < 128) {
- // passwordLength = Number(prompt("How long would you like for your password?"));
-// } else {
-//   alert("Click ok to continue");
-// }
-
-var lowerChar = confirm("Would you like lowercases in your password?");
-var lowercases = "abcdefghijklmnopqrstuvwxyz";
-if (lowerChar === true) {
-
-  for (var i = 0; i < passwordLength; i++) {
-    var randomLowercases = lowercases.charAt(Math.floor(Math.random() * lowercases.length));
-    finalPassword.push(randomLowercases);
+  var passwordLength = Number(prompt("How long would you like for your password?"));
+  console.log(passwordLength);
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    alert("Password must be between 8 and 12 characters");
+    return generatePassword();
   }
+  var lowerChar = confirm("Would you like lowercases in your password?");
+  var upperChar = confirm("Would you like uppercases in your password?");
+  var numbers = confirm("Would you like numbers in your password?");
+  var specialChar = confirm("Would you like special characters in your password?");
+
+  if (lowerChar === true) {
+    possibleChar.push(lowercases);
+    console.log(possibleChar);
+  }
+
+  if (upperChar === true) {
+    possibleChar.push(uppercases);
+    console.log(possibleChar);
+  }
+
+  if (numbers === true) {
+    possibleChar.push(nums);
+    console.log(possibleChar);
+  }
+
+  if (specialChar === true) {
+    possibleChar.push(symbols);
+    console.log(possibleChar);
+
+    for (var i = 0; i < passwordLength; i++);
+    finalPassword = Math.floor(Math.random() * possibleChar.length);
+    return finalPassword[possibleChar];
+  }
+
+
 }
 
-var upperChar = confirm("Would you like uppercases in your password?");
-var uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-if (upperChar === true) {
 
-  for (var i = 0; i < passwordLength; i++) {
-    var randomUppercases = uppercases.charAt(Math.floor(Math.random() * uppercases.length));
-    finalPassword.push(randomUppercases)
-  }
-  return finalPassword.join('');
-}
-  }
+
+
 
 // Write password to the #password input
 function writePassword() {
