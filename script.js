16b087983d7b01a28092fc73bba38edaa1825438
Sasi Patterson <sweetpea_th@hotmail.com>
2022-08-18 09:50:16 -0400

    add .join in return finalPassword to remove all the commas in between character

--- script.js
+++ script.js
@@ -5,31 +5,31 @@ var generateBtn = document.querySelector("#generate");
 
 
 function generatePassword() {
-  
-  
-  
-  var number = confirm("Would you like numbers in your password?");
-  var specialChar = confirm("Would you like special characters in your password?");
+  //var number = confirm("Would you like numbers in your password?");
+  //var specialChar = confirm("Would you like special characters in your password?");
 
-
-  
-  
   var numInput = "0123456789";
   var specialInput = " ";
   var finalPassword = [];
 
 var passwordLength = Number(prompt("How long would you like for your password?"));
-if (passwordLength < 8 || passwordLength > 128) {
+console.log(passwordLength);
+if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
   alert("Password must be between 8 and 12 characters");
-} else {
-  alert("Click ok to continue");
-}
+  return generatePassword();
+
+} 
+//else if (passwordLength > 8 || passwordLength < 128) {
+ // passwordLength = Number(prompt("How long would you like for your password?"));
+// } else {
+//   alert("Click ok to continue");
+// }
 
 var lowerChar = confirm("Would you like lowercases in your password?");
 var lowercases = "abcdefghijklmnopqrstuvwxyz";
 if (lowerChar === true) {
 
-  for (var i = 0; i < passwordLength.length; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     var randomLowercases = lowercases.charAt(Math.floor(Math.random() * lowercases.length));
     finalPassword.push(randomLowercases);
   }
@@ -39,11 +39,11 @@ var upperChar = confirm("Would you like uppercases in your password?");
 var uppercases = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 if (upperChar === true) {
 
-  for (var i = 0; i < passwordLength.length; i++) {
+  for (var i = 0; i < passwordLength; i++) {
     var randomUppercases = uppercases.charAt(Math.floor(Math.random() * uppercases.length));
     finalPassword.push(randomUppercases)
   }
-  return finalPassword;
+  return finalPassword.join('');
 }
   }
 
